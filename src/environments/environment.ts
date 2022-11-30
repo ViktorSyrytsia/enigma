9323f64253ecb2c7a317a99b95cfa8376667f1e8
Oleh-Viktor Syrytsia <osyrytsia@mynexuscare.com>
2022-11-30 23:14:15 +0200

    migrate to version 15

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'enigma-8fe15',
+    appId: '1:859324488234:web:4dabc116868d8d8e088153',
+    storageBucket: 'enigma-8fe15.appspot.com',
+    apiKey: 'AIzaSyCy-Gb-8PpSaQGRCmpvVFOGk-4aCWwqjlM',
+    authDomain: 'enigma-8fe15.firebaseapp.com',
+    messagingSenderId: '859324488234',
+    measurementId: 'G-XEHSV74BFP',
+  },
+  production: true
+};
